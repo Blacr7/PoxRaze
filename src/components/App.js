128bc31cd5f5e1df7a493eb762c861b0c1b16278
blacr7 <blacr7@mail.broward.edu>
2020-01-30 20:35:15 -0500

    Added champions and allow users to change the number of runes displayed

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,6 @@ function updateState(valueOfList){
 }
 
 class App extends Component {
-	constructor(props){
-		super(props);
-	}
 
 	updateChild(valueOfList){
 		updateState(valueOfList)
@@ -38,9 +35,10 @@ class App extends Component {
 class ControlButtons extends Component{
     render(){
         return(
-            <div class="controlButtons">
+            <div className="controlButtons">
                 {/*<button  onClick={() => updateState('listOfAbilities')} id="Abilities">Abilities</button>*/}
-                {/*<button  onClick={() => updateState('listOfChampions')} id="Champions">Champions</button>*/} 
+                {/* <button   onClick={() => updateState('listValue')} id="Previous">Previous</button> */}
+                <button   onClick={() => updateState('listOfChampions')} id="Champions">Champions</button>
                 <button   onClick={() => updateState('listOfSpells')} id="Spells">Spells</button>
                 <button   onClick={() => updateState('listOfRelics')} id="Relics">Relics</button>
                 <button   onClick={() => updateState('listOfEquips')} id="Equips">Equips</button>
@@ -53,7 +51,8 @@ class ControlButtons extends Component{
 class Runes extends Component {
     constructor(props) {
         super(props);
-        this.makeRunes = this.makeRunes.bind(this);
+        this.displayRunes = this.displayRunes.bind(this);
+        this.displayNumber = React.createRef();
         
         // initialize the lists that will be holding all of the  sites data
         this.state = {
@@ -84,75 +83,101 @@ class Runes extends Component {
         })
 
     }
+    
+
+    //for each item in a rune generate an li with a gridArea and style name for each property of a rune
+    //and populate that rune with the items contents
+    //i.e: the name property of a rune will have a gridArea of name, a class of name, and the name of the rune
+    //e.g the flavorText property for "altar of bones" will have gridArea: flavorText, class="flavorText, and flavorText: "We give this offering..." "
+    makeRunes(eachRune, listKeys){
+        let gridArea = '', hash = [], handleHtml = [], handleArrays = [];
+
+        return listKeys.map((item) => {
+            //Assign a grid area to each item for css grid formatting
+            gridArea = {gridArea: item};
+            
+            if(item === "abilitySets" || item === "startingAbilities"){
+                return ''
+            }
+            if(Array.isArray(eachRune[item])){   
+                return <li key={item} className={item} style={gridArea}>
+                    {item}: <span>{eachRune[item].map(value => {return " " + value})}</span>
+                </li>; 
+            }
+            // generate the IMG for runes based on its hash value from the API, you can determine the size from "/images/runes/ (whatever size you want) /"
+            // sizes include: lg, med, sm (not all runes have a sm size), hash also determines the sprite of the rune
+            if(item === "hash"){
+                hash = <li key={item} className={item} style={gridArea}>
+                            <div className="img-full">
+                                <img className="image" src={"https://d2aao99y1mip6n.cloudfront.net/images/runes/lg/" + eachRune[item] + ".jpg"} alt="Runes" />
+                            </div>
+                        </li>;
+                return hash
+            }
+            // convert the Html code in certain sections of runes into readable text
+            // TODO link this html code to its respective ability/condition/mechanic
+            // TODO allow the user to tell when a word is a "keyword" wrapped in html
+            if(item === "description" || item === 'tradeable' || item === 'allowRanked' || item === 'forSale'){
+                handleHtml = <li key={item} className={item} style={gridArea}>
+                            {item}: <a dangerouslySetInnerHTML={{ __html: eachRune[item]}}></a>
+                        </li>;
+                return handleHtml
+            }
+            // generate abilities  of champions, relics, and spells
+            // currently disabled for optimization and usefulness changes
+            else if(item === 1 || item === 0){
+                return <li key={item} className={item} style={gridArea}>
+                            <a dangerouslySetInnerHTML={{ __html: eachRune[item]}}></a>
+                        </li>;
+            }else{
+                // generate the rest of the items in the rune
+                return <li key={item} className={item} style={gridArea}>
+                            {item}: <span>{eachRune[item]}</span>
+                        </li>;
+            }
+        })
+    }
 
     // generate divs that contain all runes from the specified list
-    makeRunes(listOf){
-        let gridArea = '';
-        let faction = 0 ;
+    displayRunes(listOf){
+        let listOfRunes = [], eachRune = [], runeBox = [];
+        const displayNumber = this.displayNumber.current;
+
         if(listOf === undefined || listOf === any || listOf === null){
             return ''
         }else {
+            let theList = this.state[listOf];
+            //map the keys of the first item to this.keysOfTheList to give each rune within the list that lists unique headers
+            //i.e: the equips list has 14 keys in the first item (Name, NoraCost, etc...) so ALL items in the equips list
+            //will have no more than 14 keys, this is different per list e.g: Relics has 17, and conditions has 3
+            this.keysOfTheList = Object.keys(theList[0]);
+            
             // for each rune in the given list map them to the eachRune variable
-            // listOf[eachRune] is the same as listOf[i]
-            return this.state[listOf].map((eachRune) => {
-                return (
-                    //map the keys of the first rune to this.properties to give each list unique headers
-                    this.properties = Object.keys(this.state[listOf][0]),
-
+            // return this.state[listOf].map((eachRune) => {
+            
+            // for each rune within a certain range and not over the given lists length populate a runeBox
+             for (let index = 0; index < displayNumber.value && index < theList.length; index++) {
+                eachRune = theList[index];
+                runeBox = (
                     // create a div for each rune
                     <div className={`RuneBox ${listOf}`}>{
-                        //for each item generate an li with a gridarea and style name for each property of a rune
-                        //i.e: the name property will have a gridarea for name and a class of name
-                        //e.g the flavorText for altar of bones will have gridarea: flavorText and class="flavorText"
-                        
-                            this.properties.map(function(item) {
-                                gridArea = {gridArea: item};
-                                // for listOfAbilities Because the abilities API has them stored as numbers inside an object 
-                                // since only 0 and 1 are the only relevant  items break the loop once you get past 1
-                                if(item > 1){
-                                    return ''
-                                }
-                                // generate the IMG for runes based on its hash value from the API, you can determine the size from "/images/runes/ (whatever size you want) /"
-                                // hash also determines the sprite of the rune
-                                if(item === "hash"){
-                                    return <li key={item} className={item} style={gridArea}>
-                                                <div className="img-full">
-                                                    <img className="image" src={"https://d2aao99y1mip6n.cloudfront.net/images/runes/lg/" + eachRune[item] + ".jpg"} alt="Runes Image" />
-                                                </div>
-                                            </li>;
-                                }
-                                // convert the Html code in the description section of runes into neat readable text
-                                // TODO link this html code to its respective ability/condition/mechanic
-                                // TODO allow the user to tell when a word is a "keyword" wrapped in html
-                                if(item === "description" || item === 'tradeable' || item === 'allowRanked' || item === 'forSale'){
-                                    return <li key={item} className={item} style={gridArea}>
-                                                {/*convert the first letter of the item to capital: then return the value of the item*/}
-                                                {item.charAt(0).toUpperCase() + item.slice(1)}: <a dangerouslySetInnerHTML={{ __html: eachRune[item]}}></a>
-                                            </li>;
-                                }
-                                // generate abilities  of champions, relics, and spells
-                                // currently disabled for optimization and usefulness changes
-                                else if(item === 1 || item === 0){
-                                    return <li key={item} className={item} style={gridArea}>
-                                                <a dangerouslySetInnerHTML={{ __html: eachRune[item]}}></a>
-                                            </li>;
-                                }else{
-                                    // generate the rest of the items in the rune
-                                    return <li key={item} className={item} style={gridArea}>
-                                                {/*convert the first letter of the item to capital: then return the value of the item*/}
-                                                {item.charAt(0).toUpperCase() + item.slice(1)}: <span>{eachRune[item]}</span>
-                                            </li>;
-                                }
-                            })
-                        }
+                        this.makeRunes(eachRune, this.keysOfTheList)
+                    }
                     </div>
-                )
-            })
+                )        
+                listOfRunes.push(runeBox);
+            }
+            return listOfRunes;
         }
     }
 
     render(){
-        return <div ref='placement' id="runesPlacement">{ this.makeRunes(this.state.valueOfList)}</div>
+        return <div ref='placement' id="runesPlacement">
+            
+            <input ref={this.displayNumber} type="number" id="Next" defaultValue="10" min="1" max="1500"></input>
+
+            {this.displayRunes(this.state.valueOfList)}
+        </div>
     }
 }
 
