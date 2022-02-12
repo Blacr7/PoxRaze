b7424317b2fb35541c2c353c5530576ba4b27e36
blacr7 <blacr7@mail.broward.edu>
2022-02-12 12:02:41 -0500

    Revert "Merge branch 'gh-pages' of https://github.com/Blacr7/PoxRaze into gh-pages"
    This reverts commit 3c6d0f2b449eb7646e9b9bd4d3fba5b553df03f7, reversing
    changes made to 9484610d4ed1a3154f31ac7a1e5f1ea1228567bb.

---
+++ src/components/Runes/Api files/main.js
@@ -0,0 +1,14 @@
+import { conditions} from './condition.json';
+import { mechanics} from './mechanics.json';
+import { champs, spells, equips, relics} from './champs.json';
+import {abilities} from './abilities.json'
+
+export{
+    conditions,
+    mechanics,
+    champs,
+    spells,
+    equips,
+    relics,
+    abilities
+}
